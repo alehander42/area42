929d0a6507d5aa5a83caf4406fda493e9519c9bb
Alexander Ivanov <alehander42@gmail.com>
2018-03-19 10:31:10 +0200

    Final dsl

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # area42
 
 compiler dsl
+
+## parser
+
+the first mini-dsl of area42
+
+goals:
+
+* easy to read, not very *magical*
+* good interop with next area42 dsl-s
+* generation of correct, but very simple parsers
+* non-optimized output: for optimized use other speedy libs
+* maybe: generate parsers in various languages one day based on the same nim dsl
+
+## next
+
+TODO
+
--- parser.nim
+++ parser.nim
@@ -3,9 +3,17 @@ import macros
 macro grammar(rules: untyped): untyped =
   echo rules.treerepr
 
+# ws nl leftParen rightParen are builtins
+
 grammar:
-  integer:    i@regex(r"[0-9]+")
-  label:      label@regex(r"[a-z\-\+\=\*]+")
-  element:    integer | label
-  expression: element | call
-  call:       leftParen function@expression ws args@join(expression, ws) rightParen
+  program =>              *(topLevel <- topLevel nl) # topLevel will be a seq with all the nodes
+  topLevel =>             function | call 
+  function =>             typeDefinition functionDefinition
+  call =>                 leftParen function:expression ws args:join(expression, ws) rightParen
+  typeDefinition =>       leftParen "type" ws types:join(typ, ws) rightParen
+  functionDefinition =>   leftParen "function" ws name:label ws leftParen functionArgs:join(label, ws) rightParen expression:expression rightParen
+  typ =>                  typeLabel:regex(r"[A-Z][a-zA-Z\-]*")
+  expression =>           element | call
+  element =>              integer | label
+  label =>                label:regex(r"[a-z\-\+\=\*]+")
+  integer =>              i:regex(r"[0-9]+")
